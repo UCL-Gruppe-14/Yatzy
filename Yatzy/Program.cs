b3d4e529a73b14b611a6e4f8905cde069bd27912
Peter Ilsted <pibe55630@edu.ucl.dk>
2024-02-16 19:49:25 +0100

    Add project files.

---
+++ Yatzy/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Yatzy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int t1, t2, t3, t4, t5;
+            
+            for (int i = 0; i <= 10; i++)
+            {
+                t1 = r.Next(1, 6);
+                t2 = r.Next(1, 6);
+                t3 = r.Next(1, 6);
+                t4 = r.Next(1, 6);
+                t5 = r.Next(1, 6);
+                Console.WriteLine("Terningen ruller, og bliver: {0} {1} {2} {3} {4}", t1, t2, t3, t4, t5);
+            }
+            
+        }
+    }
+}
