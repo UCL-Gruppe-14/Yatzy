456bf908f2d0ebc7bfc2f0c9868d89ef9b75b5b7
Peter Ilsted <pibe55630@edu.ucl.dk>
2024-02-20 09:15:37 +0100

    Added Class

---
+++ Yatzy/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzy
+{
+    internal class Player
+    {
+        String Navn;
+        int Enere;
+        int Toere;
+        int Treere;
+        int Firere;
+        int Femmere;
+        int Seksere;
+        int Sum = Enere + Toere + Treere + Firere + Femmere + Seksere;
+        int Bonus;
+        int etPar;
+        int toPar;
+        int treEns;
+        int fireEns;
+        int lilleStraight;
+        int storStraight;
+        int Hus;
+        int Chance;
+        int Yatzy;
+        int Total = Sum + Bonus + etPar + toPar + treEns + fireEns + lilleStraight + storStraight + Hus + Chance + Yatzy;
+    }
+}
--- Yatzy/Program.cs
+++ Yatzy/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Yatzy
+﻿using System.Xml.Schema;
+
+namespace Yatzy
 {
     internal class Program
     {
@@ -6,17 +8,21 @@
         {
             Random r = new Random();
             int t1, t2, t3, t4, t5;
-            
-            for (int i = 0; i <= 10; i++)
-            {
-                t1 = r.Next(1, 6);
-                t2 = r.Next(1, 6);
-                t3 = r.Next(1, 6);
-                t4 = r.Next(1, 6);
-                t5 = r.Next(1, 6);
+            int valg;
+            int terning;
+
+            t1 = r.Next(1, 6);
+            t2 = r.Next(1, 6);
+            t3 = r.Next(1, 6);
+            t4 = r.Next(1, 6);
+            t5 = r.Next(1, 6);
+            for (int i = 0; i <= 3; i++)
+            {                
                 Console.WriteLine("Terningen ruller, og bliver: {0} {1} {2} {3} {4}", t1, t2, t3, t4, t5);
-            }
-            
+                Console.WriteLine("Hvad vil du foretage dig?");
+                valg = int.Parse(Console.ReadLine());
+                
+            }            
         }
     }
 }
