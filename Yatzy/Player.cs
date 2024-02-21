df942e43073a98f5c59c5b35e48beaa8da80be32
Peter Ilsted <pibe55630@edu.ucl.dk>
2024-02-21 06:33:45 +0100

    Startet op på spiller 1, 2 og 3 i main

--- Yatzy/Player.cs
+++ Yatzy/Player.cs
@@ -9,26 +9,28 @@ using System.Threading.Tasks;
 
 namespace Yatzy
 {
-    internal class Player
+    public class Player
     {
-        String Navn;
-        int Enere;
-        int Toere;
-        int Treere;
-        int Firere;
-        int Femmere;
-        int Seksere;
-        int Sum = Enere + Toere + Treere + Firere + Femmere + Seksere;
-        int Bonus;
-        int etPar;
-        int toPar;
-        int treEns;
-        int fireEns;
-        int lilleStraight;
-        int storStraight;
-        int Hus;
-        int Chance;
-        int Yatzy;
-        int Total = Sum + Bonus + etPar + toPar + treEns + fireEns + lilleStraight + storStraight + Hus + Chance + Yatzy;
+        public String Navn;
+        public int Enere;
+        public int Toere;
+        public int Treere;
+        public int Firere;
+        public int Femmere;
+        public int Seksere;
+        public int Sum = Enere + Toere + Treere + Firere + Femmere + Seksere;
+        public int Bonus;
+        public int etPar;
+        public int toPar;
+        public int treEns;
+        public int fireEns;
+        public int lilleStraight;
+        public int storStraight;
+        public int Hus;
+        public int Chance;
+        public int Yatzy;
+        public int Total = Sum + Bonus + etPar + toPar + treEns + fireEns + lilleStraight + storStraight + Hus + Chance + Yatzy;
+
+        
     }
 }
--- Yatzy/Program.cs
+++ Yatzy/Program.cs
@@ -7,22 +7,56 @@ namespace Yatzy
         static void Main(string[] args)
         {
             Random r = new Random();
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Player p3 = new Player();
+            int antalSpillere;
             int t1, t2, t3, t4, t5;
             int valg;
             int terning;
+            bool rigtigtValg = true;
 
             t1 = r.Next(1, 6);
             t2 = r.Next(1, 6);
             t3 = r.Next(1, 6);
             t4 = r.Next(1, 6);
             t5 = r.Next(1, 6);
-            for (int i = 0; i <= 3; i++)
-            {                
-                Console.WriteLine("Terningen ruller, og bliver: {0} {1} {2} {3} {4}", t1, t2, t3, t4, t5);
-                Console.WriteLine("Hvad vil du foretage dig?");
-                valg = int.Parse(Console.ReadLine());
-                
-            }            
+            do
+            {
+                Console.WriteLine("Velkommen til YATZY\nSpillet der samler og knækker familier og venskaber!");
+                Console.Write("Vælg antal spillere: ");
+                antalSpillere = int.Parse(Console.ReadLine());
+                switch (antalSpillere)
+                {
+                    case 1:
+                        Console.Write("Spiller 1 - Navn: ");
+                        p1.Navn = Console.ReadLine();
+                        Console.Write("Spiller 2 - Navn: ");
+                        p2.Navn = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Spiller 1 - Navn: ");
+                        p1.Navn = Console.ReadLine();
+                        Console.Write("Spiller 2 - Navn: ");
+                        p2.Navn = Console.ReadLine();
+                        Console.Write("Spiller 3 - Navn: ");
+                        p3.Navn = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Forkert Input");
+                        break;
+                }
+
+                Console.WriteLine("{0}'s tur")
+                for (int i = 0; i <= 3; i++)
+                {
+                    Console.WriteLine("Terningen ruller, og bliver: {0}, {1}, {2}, {3}, {4}", t1, t2, t3, t4, t5);
+                    Console.WriteLine("Hvad vil du foretage dig?");
+                    valg = int.Parse(Console.ReadLine());
+
+                }
+            } while (rigtigtValg);
+            
         }
     }
 }
