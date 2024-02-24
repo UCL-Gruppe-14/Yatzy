8e9effffac0f8bc190f8091ea07aad06d9776275
Peter Ilsted <pibe55630@edu.ucl.dk>
2024-02-24 08:21:14 +0100

    Oprettet terningeslag metode, og metode til at printe det ud

--- Yatzy/Player.cs
+++ Yatzy/Player.cs
@@ -4,33 +4,67 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Yatzy
 {
-    public class Player
+    internal class Player
     {
-        public String Navn;
-        public int Enere;
-        public int Toere;
-        public int Treere;
-        public int Firere;
-        public int Femmere;
-        public int Seksere;
-        public int Sum = Enere + Toere + Treere + Firere + Femmere + Seksere;
-        public int Bonus;
-        public int etPar;
-        public int toPar;
-        public int treEns;
+
+        Random r = new Random();
+
+        int r1, r2, r3, r4, r5;
+        int[] Tur = new int[5];
+
+        private String Navn;
+        private int Enere;
+        private int Toere;
+        private int Treere;
+        private int Firere;
+        private int Femmere;
+        private int Seksere;
+        //private int Sum = Enere + Toere + Treere + Firere + Femmere + Seksere;
+        private int Bonus;
+        private int etPar;
+        private int toPar;
+        private int treEns;
         public int fireEns;
         public int lilleStraight;
         public int storStraight;
         public int Hus;
         public int Chance;
         public int Yatzy;
-        public int Total = Sum + Bonus + etPar + toPar + treEns + fireEns + lilleStraight + storStraight + Hus + Chance + Yatzy;
+        //public int Total = Sum + Bonus + etPar + toPar + treEns + fireEns + lilleStraight + storStraight + Hus + Chance + Yatzy;
 
-        
+        public void setName()
+        {
+            Console.WriteLine("Hvad er dit navn: ");
+            Navn = Console.ReadLine();
+        }
+        public void ChangeTur()
+        {
+            r1 = r.Next(1, 6);
+            r2 = r.Next(1, 6);
+            r3 = r.Next(1, 6);
+            r4 = r.Next(1, 6);
+            r5 = r.Next(1, 6);
+            Tur[0] = r1;
+            Tur[1] = r2;
+            Tur[2] = r3;
+            Tur[3] = r4;
+            Tur[4] = r5;
+        }
+        public void Slag()
+        {
+            for(int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Terningerul: " + Tur[i]);
+            }
+        }
+
+
+
     }
 }
--- Yatzy/Program.cs
+++ Yatzy/Program.cs
@@ -6,7 +6,7 @@ namespace Yatzy
     {
         static void Main(string[] args)
         {
-            Random r = new Random();
+            Random q = new Random();
             Player p1 = new Player();
             Player p2 = new Player();
             Player p3 = new Player();
@@ -16,41 +16,46 @@ namespace Yatzy
             int terning;
             bool rigtigtValg = true;
 
-            t1 = r.Next(1, 6);
-            t2 = r.Next(1, 6);
-            t3 = r.Next(1, 6);
-            t4 = r.Next(1, 6);
-            t5 = r.Next(1, 6);
+            t1 = q.Next(1, 6);
+
+            p1.ChangeTur();
+            p1.Slag();
+
+
+            
+            
             do
             {
                 Console.WriteLine("Velkommen til YATZY\nSpillet der samler og knækker familier og venskaber!");
                 Console.Write("Vælg antal spillere: ");
                 antalSpillere = int.Parse(Console.ReadLine());
+                int antalSpiller= Convert.ToInt32(Console.ReadLine());     
                 switch (antalSpillere)
                 {
                     case 1:
-                        Console.Write("Spiller 1 - Navn: ");
-                        p1.Navn = Console.ReadLine();
-                        Console.Write("Spiller 2 - Navn: ");
-                        p2.Navn = Console.ReadLine();
+                        Console.WriteLine("Spiller 1");
+                        p1.setName();
+                        Console.WriteLine("Spiller 2");
+                        p2.setName();
+                        Console.WriteLine("Spiller 3");
                         break;
                     case 2:
-                        Console.Write("Spiller 1 - Navn: ");
-                        p1.Navn = Console.ReadLine();
-                        Console.Write("Spiller 2 - Navn: ");
-                        p2.Navn = Console.ReadLine();
-                        Console.Write("Spiller 3 - Navn: ");
-                        p3.Navn = Console.ReadLine();
+                        Console.WriteLine("Spiller 1");
+                        p1.setName();
+                        Console.WriteLine("Spiller 2");
+                        p2.setName();
+                        Console.WriteLine("Spiller 3");
+                        p3.setName();
                         break;
                     default:
                         Console.WriteLine("Forkert Input");
                         break;
                 }
 
-                Console.WriteLine("{0}'s tur")
+                Console.WriteLine("{0}'s tur");
                 for (int i = 0; i <= 3; i++)
                 {
-                    Console.WriteLine("Terningen ruller, og bliver: {0}, {1}, {2}, {3}, {4}", t1, t2, t3, t4, t5);
+                    Console.WriteLine("Terningen ruller, og bliver: {0}, {1}, {2}, {3}, {4}", t1);
                     Console.WriteLine("Hvad vil du foretage dig?");
                     valg = int.Parse(Console.ReadLine());
 
