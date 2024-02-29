ad30f006451d2020f11cb4738150207dd2c371bc
Peter Ilsted <pibe55630@edu.ucl.dk>
2024-02-29 21:18:58 +0100

    Lavet en try catch ved valg af antal spillere

--- Yatzy/Program.cs
+++ Yatzy/Program.cs
@@ -9,63 +9,72 @@ namespace Yatzy
             Player p1 = new Player();
             Player p2 = new Player();
             Player p3 = new Player();
-            int antalSpillere;
+            int antalSpillere = 0;
 
             int valg;
             int terning;
-            bool rigtigtValg = true;
+            bool AntalSpillere = true;
 
-            //p1.ChangeTur();
-            //p1.Slag();
-
-            /*Console.WriteLine("Velkommen til YATZY\nSpillet der samler og knækker familier og venskaber!");
-            Console.Write("Vælg antal spillere: ");
-            antalSpillere = int.Parse(Console.ReadLine());
-            switch (antalSpillere)
+            Console.WriteLine("Velkommen til YATZY\nSpillet der samler og knækker familier og venskaber!");
+            do
             {
-                case 1:
-                    Console.WriteLine("Spiller 1");
-                    p1.setName();
-                    Console.WriteLine("Spiller 2");
-                    p2.setName();
-                    Console.WriteLine("Spiller 3");
-                    break;
-                case 2:
-                    Console.WriteLine("Spiller 1");
-                    p1.setName();
-                    Console.WriteLine("Spiller 2");
-                    p2.setName();
-                    Console.WriteLine("Spiller 3");
-                    p3.setName();
-                    break;
-                default:
-                    Console.WriteLine("Forkert Input");
-                    break;
-            }*/
-
-
-            //p1.setName();
-            //if (antalSpillere == 1)
-            {
-                for (int i = 1; i <= 15; i++)
+                Console.Write("Vælg antal spillere: ");
+                try
                 {
-                    //Console.WriteLine(p1.Navn + "'s tur");
-                    p1.ChangeTur();
-                    p1.Slag();
-                    for (int x = 1; x <= 2; x++)
-                    {
-                       p1.Omslag();
-                       p1.Slag();
-                    }
-                    p1.PointTavle();
-                    do
-                    {
-                        p1.Point();
-                        p1.PointTavle();
-                    } while (p1.tildelt == false);
+                    antalSpillere = int.Parse(Console.ReadLine());
                 }
-                Console.WriteLine("Spillet er slut, pointsne er talt op, og x vandt med: {0} point", p1.PointTotal());
+                catch (Exception e)
+                {
+                    Console.WriteLine("Forkert input, skriv et tal");
+                }
+                
+                switch (antalSpillere)
+                {
+                    case 1:
+                        Console.WriteLine("Spiller 1");
+                        p1.setName();
+                        AntalSpillere= false;
+                        break;
+                    case 2:
+                        Console.WriteLine("Spiller 1");
+                        p1.setName();
+                        Console.WriteLine("Spiller 2");
+                        p2.setName();
+                        AntalSpillere= false;
+                        break;
+                    case 3:
+                        Console.WriteLine("Spiller 1");
+                        p1.setName();
+                        Console.WriteLine("Spiller 2");
+                        p2.setName();
+                        Console.WriteLine("Spiller 3");
+                        p3.setName();
+                        AntalSpillere= false;
+                        break;
+                    default:
+                        Console.WriteLine("Forkert input:\nVælg mellem 1 - 3 spillere.");
+                        break;
+                }
+            } while (AntalSpillere == true);
+
+            for (int i = 1; i <= 15; i++)
+            {
+                Console.WriteLine(p1.Navn + "'s tur");
+                p1.ChangeTur();
+                p1.Slag();
+                for (int x = 1; x <= 2; x++)
+                {
+                    p1.Omslag();
+                    p1.Slag();
+                }
+                p1.PointTavle();
+                do
+                {
+                    p1.Point();
+                    p1.PointTavle();
+                } while (p1.tildelt == false);
             }
+            Console.WriteLine("Spillet er slut, pointsne er talt op, og x vandt med: {0} point", p1.PointTotal());
         }
     }
 }
